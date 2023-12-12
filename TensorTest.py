e62d0cb759386dd2f5636948f3aa5fb27716b2f5
Boris Verhaar <borisverhaar@gmail.com>
2023-12-12 16:25:54 +0100

    added tensorflow start

---
+++ TensorTest.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+from tfdiffeq import odeint
+import pandas as pd
+import numpy as np
+
+print(f"using TensorFlow version {tf.__version__}")
+
+data = pd.read_csv("toydatafixed.csv", delimiter=';')
+t_tensor = tf.convert_to_tensor(data['t'].values, dtype=tf.float32)
+features_tensor = tf.convert_to_tensor(data[['speed', 'angle']].values, dtype=tf.float32)
+min_vals = tf.reduce_min(features_tensor, axis=0)
+max_vals = tf.reduce_max(features_tensor, axis=0)
+features_tensor = (features_tensor - min_vals) / (max_vals - min_vals)
+num_feat = features_tensor.shape[1]
+
+def simple_split(train_dur, val_dur):
+    split_train_dur = train_dur
+    split_val_dur = val_dur
+
+    split_train = int(split_train_dur / 0.005)  
+    split_val = int((split_val_dur + split_train_dur) / 0.005)
+    train_data = (t_tensor[:split_train], features_tensor[:split_train])
+    val_data = (t_tensor[split_train:split_val], features_tensor[split_train:split_val])
+    test_data = (t_tensor[split_val:], features_tensor[split_val:])
+
+def val_shift_split(train_dur, val_shift):
+    split_train_dur = train_dur
+    shift_val_dur = val_shift
+
+    split_train = int(split_train_dur / 0.005)  
+    shift_val = int(shift_val_dur  / 0.005)
+
+    train_data = (t_tensor[:split_train], features_tensor[:split_train])
+    val_data = (t_tensor[shift_val:split_train + shift_val], features_tensor[shift_val:split_train + shift_val])
+    test_data = (t_tensor[split_train:], features_tensor[split_train:])
+    return train_data, val_data, test_data
+
+
+class ODEFunc(tf.keras.Model):
+
+    def __init__(self, **kwargs):
+        super(ODEFunc, self).__init__(**kwargs)
+
+        self.x = tf.keras.layers.Dense(50, activation='tanh',
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
+        self.y = tf.keras.layers.Dense(2,
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal(stddev=0.1))
+
+    def call(self, t, y):
+        y = tf.cast(y, tf.float32)
+        x = self.x(y ** 3)
+        y = self.y(x)
+        return y
+
+
+#generated with copilot and not adjust for NODE use
+def train_model(data, data2,lr = 0.01, num_epochs=10):
+    
+    t, features = data
+    val_t, val_features = data2
+
+
+    loss_fn = tf.keras.losses.MeanSquaredError()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+    net = ODEFunc()
+
+    for epoch in range(num_epochs):
+        with tf.GradientTape() as tape:
+            pred_y = odeint(net, features[0], t)
+            loss = loss_fn(pred_y, features)
+
+        grads = tape.gradient(loss, net.variables)
+        grad_vars = zip(grads, net.variables)
+        optimizer.apply_gradients(grad_vars)
+
+
+        print(f"Epoch {epoch+1}, Loss: {loss.numpy()}")
+
+
+
+# train_data, val_data, test_data = simple_split(2, 2)  # Assuming you want to use the simple_split() function
+train_data, val_data, test_data = val_shift_split(3, .3)  # Assuming you want to use the val_shift_split() function
+
+train_model(train_data, val_data)
+
--- TorchTest.py
+++ TorchTest.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torchdiffeq import odeint_adjoint as odeint
 import pandas as pd
 from time import time
 import matplotlib.pyplot as plt
-import torch.optim as optim
 
-print(f"gpu availible {torch.cuda.is_available()}")
+
+print(f"gpu availible: {torch.cuda.is_available()}")
 use_cuda = False
 if use_cuda and torch.cuda.is_available():
     device = torch.device("cuda")
@@ -73,14 +74,17 @@ class ODEFunc(nn.Module):
             nn.Tanh(),
             nn.Linear(N_neurons, N_neurons),
             nn.Tanh(),
-            nn.Linear(N_neurons, N_neurons),
-            nn.Tanh(),
             nn.Linear(N_neurons, num_feat)
         )
+        
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=0.1)
+                nn.init.constant_(m.bias, val=0)
 
     def forward(self, t, y):
         y = y.to(device)
-        return self.net(y)
+        return self.net(y**3)
 
 
 #loss functions
@@ -108,7 +112,7 @@ def mean_fourth_power_error(y_true, y_pred):
 #cross entropy loss
 
 @tictoc
-def trainmodel(data, data2, learning_rate, num_epochs, num_neurons, rel_tol, abs_tol, live_plot):
+def trainmodel(data, data2, learning_rate, num_epochs, num_neurons, rel_tol=1e-7, abs_tol=1e-9, live_plot=False, intermidiate_prediction=False):
     """
     Trains a neural network model using the NODE (Neural Ordinary Differential Equations) approach.
 
@@ -121,6 +125,7 @@ def trainmodel(data, data2, learning_rate, num_epochs, num_neurons, rel_tol, abs
         rel_tol (float): The relative tolerance for the ODE solver.
         abs_tol (float): The absolute tolerance for the ODE solver.
         live_plot (bool): Whether to enable live plotting of the training and validation losses.
+        intermidiate_prediction (bool): Whether to perform intermediate predictions during training.
 
     Returns:
         tuple: A tuple containing the trained neural network model and a tuple of training and validation losses.
@@ -161,7 +166,7 @@ def trainmodel(data, data2, learning_rate, num_epochs, num_neurons, rel_tol, abs
         
         else:
             optimizer.zero_grad()
-            pred_y = odeint(net, features[0], t, rtol=rel_tol, atol=abs_tol, method="dopri8")
+            pred_y = odeint(net, features[0], t, rtol=rel_tol, atol=abs_tol, method="dopri5")
             loss = loss_function(pred_y, features)
             loss.backward()
             optimizer.step()
@@ -173,16 +178,19 @@ def trainmodel(data, data2, learning_rate, num_epochs, num_neurons, rel_tol, abs
         train_losses.append(loss.item())
         val_losses.append(loss_val.item())
 
+        print(f"Epoch {epoch+1}: loss = {loss.item()}, val_loss = {loss_val.item()}")
+
         if live_plot and epoch % 5 == 4:
             line1.set_data(range(epoch + 1), train_losses)
             line2.set_data(range(epoch + 1), val_losses)
             ax.relim()  # Recalculate limits
             ax.autoscale_view(True,True,True)  # Autoscale
             plt.draw()
-            plt.pause(0.1)  # Pause to update the plot
+            plt.pause(0.2)  # Pause to update the plot
 
-        print(f"Epoch {epoch+1}: loss = {loss.item()}, val_loss = {loss_val.item()}")
-        if epoch % 50 == 49:
+        
+
+        if intermidiate_prediction and epoch % 30 == 29:
             intermidiate_prediction(net, epoch)
     
     return net, (train_losses, val_losses)
@@ -214,7 +222,7 @@ def intermidiate_prediction(network, epoch):
 
 
 
-network, losses = trainmodel(train_data, val_data, 0.01, 50, 50, 1e-7, 1e-5, False)
+network, losses = trainmodel(train_data, val_data, 0.01, 150, 50, 1e-7, 1e-5)
 
 
 
@@ -222,6 +230,7 @@ plot_data = False
 plot_actual_vs_predicted = False
 plot_actual_vs_predicted_full = True
 plot_training_vs_validation = True
+plot_phase_space = True
 
 
 
@@ -293,6 +302,24 @@ if plot_training_vs_validation:
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
+
+
+if plot_phase_space:
+    with torch.no_grad():
+        predicted_full = odeint(network, tensor_data[1][0], tensor_data[0])
+    evaluation_loss_full = MSELoss(predicted_full, tensor_data[1]).item()
+    print(f"Mean Squared Error Loss Full: {evaluation_loss_full}")
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(tensor_data[1][:, 0].cpu().detach().numpy(), tensor_data[1][:, 1].cpu().detach().numpy(), label='Actual')
+    plt.plot(predicted_full[:, 0].cpu().detach().numpy(), predicted_full[:, 1].cpu().detach().numpy(), label='Predicted', linestyle='--')
+    plt.xlabel('Speed')
+    plt.ylabel('Angle')
+    plt.title('Phase Space: Speed vs Angle')
+    plt.legend()
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.show()
+
 plt.show()
 
 
