de1cbb2a09d3580d6015b4f85ae4637051aa3c04
Boris Verhaar <borisverhaar@gmail.com>
2024-01-17 16:01:50 +0100

    moving stuff around

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@
 
 # from tensorflow_models.Tensor_base_model import main as tensor_base_model
 from tensorflow_models.TensorTest import main as tensor_test_model
-from tensorflow_models.Torch_Toy_Model import main as torch_toy_test_model
+from pytorch_models.Torch_Toy_Model import main as torch_toy_model
 #run everything from here
 
-def main():=
+def main():
     # torch_base_model()
     
-    torch_toy_test_model(num_epochs=30, learning_rate=0.0003) #, intermediate_pred_freq=300)
+    torch_toy_model(num_epochs=30, learning_rate=0.0003) #, intermediate_pred_freq=300)
     tensor_test_model(num_epochs=30, learning_rate=0.0003)
     #testing git
 
