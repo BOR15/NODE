07685a8e548d931af7aad69bc6a7fe72a5b43442
laetichou <laetitia.guerin2003@gmail.com>
2024-01-25 18:54:36 +0100

    Fixed Frechet distance

--- src/tools/Metrics.py
+++ src/tools/Metrics.py
@@ -16,26 +16,31 @@ def frechet_distance(P, Q, metric='euclidean'):
     y_true= P[1]
     d = 0
     # print(y_true[:,0].reshape(-1, 1).shape)
-    for i in range(5):
+    for ii in range(5):
         # print(t.reshape(-1, 1).shape)
-        true_P = torch.stack((t, y_true[:, i]), dim=1)
-        # print(true_P.shape)
-        pred_Q = torch.stack((t, Q[:, i]), dim=1)
+        true_P = torch.stack((t, y_true[:, ii]), dim=1)
+        # print(true_P)
+        pred_Q = torch.stack((t, Q[:, ii]), dim=1)
 
-        # Compute pairwise distances
-        couplings = cdist(true_P.T, pred_Q.T, metric= metric)
-        # print(couplings)
-        #might not need to transpose
-        # couplings = cdist(P, Q, metric= metric)
-
+        # Compute pairwise distances, should not transpose
+        couplings = cdist(true_P, pred_Q, metric= metric)
+        # print("Couplings: ", couplings)
+        # print("Couplings shape: ", couplings.shape)
 
         m, n = couplings.shape
 
-        # Update couplings matrix
-        for i in range(1, m):
-            couplings[i, 0] = max(couplings[i-1, 0], couplings[i, 0])
+        # Clip the values in the coupling matrix at the average value of all distances
+        avg =0
+        for i in range(m):
+            avg += np.mean(couplings[i, :])
+        couplings = np.clip(couplings, a_min=None, a_max=avg/m)
+
+        # Update couplings matrix; This loop is not needed because our pred and true data are always the same length.
+        # for i in range(1, m):
+        #     couplings[i, 0] = max(couplings[i-1, 0], couplings[i, 0])
 
         for j in range(1, n):
+            couplings[j, 0] = max(couplings[j-1, 0], couplings[j, 0])
             couplings[0, j] = max(couplings[0, j-1], couplings[0, j])
             for i in range(1, m):
                 carried_coupling = min(couplings[i-1, j-1], couplings[i, j-1], couplings[i-1, j])
