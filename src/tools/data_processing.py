70edd828a0c9fd95d451bcfdbd5af14ac1c47243
laetichou <laetitia.guerin2003@gmail.com>
2024-01-25 21:16:24 +0100

    Cleaned up data_processing file and added type hints

--- src/tools/data_processing.py
+++ src/tools/data_processing.py
@@ -6,24 +6,8 @@ import tensorflow as tf
 
 from scipy.interpolate import interp1d
 
-"""
-READ THIS:
 
-This stuff hasnt been worked on yet, right now its just an old copy of toydata_processing.py 
-
-I have now started on this but still mess 
-"""
-
-# Testing testing how to commit and push 
-berend_path = r"C:\Users\Mieke\Documents\GitHub\NODE\Input_Data\Raw_Data\Dynamics40h17.csv"
-boris_path = "NODE/Input_Data/Raw_data/Dynamics40h17.csv"
-#laetitia_path = "/Users/laetitiaguerin/Library/CloudStorage/OneDrive-Personal/Documents/BSc Nanobiology/Year 4/Capstone Project/Github repository/NODE/Input_Data/Raw_Data/Dynamics40h17.csv"
-
-berend_scufed = r"C:\Users\Mieke\Documents\GitHub\NODE\Input_Data\real_data_scuffed2.pt"
-boris_scufed = "real_data_scuffed2.pt"
-
-
-def load_data(filename, shift=0):
+def load_data(filename: str, shift: int=0) -> tuple[torch.Tensor, torch.Tensor]:
     '''
     Loads the data from a CSV file.
     Drops duplicates in the time axis, keeping the last one only. 
@@ -49,11 +33,10 @@ def load_data(filename, shift=0):
     return t_tensor, features_tensor
 
 
-# def load_data_avg_duplicates(filename, shift=0, start=300):
+# def load_data_avg_duplicates(filename:str, shift:int =0, start:int =300) -> tuple[torch.Tensor, torch.Tensor]:
 #     '''
 #     Loads the data from a CSV file. 
 #     Replaces duplicate time points by their average value.
-#     Normalizes the features with mean 0 and std 1.
 #     Returns two tensors: time and features.
 #     '''
 #     # Import data
@@ -70,14 +53,13 @@ def load_data(filename, shift=0):
 #     # Defining tensors
 #     t_tensor = torch.tensor(data.iloc[start:, 1].values, dtype=torch.float32)
 #     features_tensor = torch.tensor(data.iloc[start:, 2+shift:27+shift:5].values, dtype=torch.float32)
-#     print(features_tensor.shape, t_tensor.shape)
-#     print(features_tensor[0], t_tensor[0])
+#     print("Time tensor shape:", t_tensor.shape, "; Features tensor shape:", features_tensor.shape)
+#     print("Time tensor: ", t_tensor[0], "; Features tensor: ", features_tensor[0])
 
-#     features_tensor = normalize_data_mean_0(features_tensor)
 #     return t_tensor, features_tensor
 
 
-def clean_start(data: tuple[torch.Tensor, torch.Tensor], start_idx: int):
+def clean_start(data: tuple[torch.Tensor, torch.Tensor], start_idx: int) -> tuple[torch.Tensor, torch.Tensor]:
     '''Cuts off start of signal up to given index and realigns time tensor from 0.'''
     t_tensor, features_tensor = data
 
@@ -88,33 +70,31 @@ def clean_start(data: tuple[torch.Tensor, torch.Tensor], start_idx: int):
     return time_aligned, features_clean
 
 
-def linspace_time_series(t_tensor):
-    '''
-    Output new tensor with evenly-spaced time-points.
-    '''
+def linspace_time_series(t_tensor: torch.Tensor) -> torch.Tensor:
+    '''Return new time tensor with linearly-spaced points.'''
     t_tensor_linspaced = torch.tensor(np.linspace(t_tensor[0], t_tensor[-1], len(t_tensor)))
     return t_tensor_linspaced
 
 
-def get_split_indexes(filepath, train_dur, val_dur=None):
-    '''Return indexes in time series corresponding to train (and val) time(s).'''
-    t_tensor, features = torch.load(filepath)
+# def get_split_indexes(filepath, train_dur, val_dur=None):
+#     '''Return indexes in time series corresponding to train (and val) time(s).'''
+#     t_tensor, features = torch.load(filepath)
 
-    # Round times to 1 decimal point
-    train_time = round(train_dur * 10) / 10  
-    time_rounded = torch.round(t_tensor * 10) / 10
+#     # Round times to 1 decimal point
+#     train_time = round(train_dur * 10) / 10  
+#     time_rounded = torch.round(t_tensor * 10) / 10
 
-    train_index = [index for index, value in enumerate(time_rounded) if train_time == value][0]
+#     train_index = [index for index, value in enumerate(time_rounded) if train_time == value][0]
 
-    # Validation time is not used at the moment
-    if val_dur:
-        val_time = round((train_dur + val_dur) * 10) / 10
-        val_index = [index for index, value in enumerate(time_rounded) if val_time == value][0]
+#     # Validation time is not used at the moment
+#     if val_dur:
+#         val_time = round((train_dur + val_dur) * 10) / 10
+#         val_index = [index for index, value in enumerate(time_rounded) if val_time == value][0]
 
-    return train_index, val_index
+#     return train_index, val_index
 
 
-def interpolate_features(data, num_sample_points=1024, interpolation_kind='linear'):
+def interpolate_features(data: tuple[torch.Tensor, torch.Tensor], num_sample_points:int = 1024, interpolation_kind:str='linear') -> tuple[torch.Tensor, torch.Tensor]:
     """
     Linearly interpolate missing values in the features_tensor based on the time points in t_tensor.
     Sample `num_sample_points` evenly spaced points from the interpolated result.
@@ -138,51 +118,31 @@ def interpolate_features(data, num_sample_points=1024, interpolation_kind='linea
     return sampled_timepoints, interpolated_features_tensor
 
 
-def remove_spikes(t_tensor, features_tensor, spike_threshold=0.1):
-    """
-    Remove the first few data points up to the first discontinuity spike after interpolation.
-    The threshold defines the maximum allowed relative change in feature values.
-    """
-    # Convert tensors to numpy arrays
-    t_np = t_tensor.numpy()
-    features_np = features_tensor.numpy()
-
-    # Calculate relative changes in feature values
-    relative_changes = np.abs((features_np[1:] - features_np[:-1]) / features_np[:-1])
-
-    # Find the index of the first spike exceeding the threshold
-    spike_idx = np.argmax(relative_changes > spike_threshold)
-
-    # Remove data up to the spike
-    t_tensor_no_spikes = t_tensor[spike_idx:]
-    features_tensor_no_spikes = features_tensor[spike_idx:]
-
-    return t_tensor_no_spikes, features_tensor_no_spikes
-
-# #removes first few time points that are the same after interpolation
-# def remove_redundant_data(t_tensor, features_tensor):
+# def remove_spikes(t_tensor: torch.Tensor, features_tensor:torch.Tensor, spike_threshold:float = 0.1) -> tuple[torch.Tensor, torch.Tensor]:
+#     """
+#     Remove the first few data points up to the first discontinuity spike after interpolation.
+#     The threshold defines the maximum allowed relative change in feature values.
+#     """
 #     # Convert tensors to numpy arrays
 #     t_np = t_tensor.numpy()
 #     features_np = features_tensor.numpy()
 
-#     # Find the index of the first non-redundant time point
-#     non_redundant_start_idx = 0
-#     for i in range(1, len(t_np)):
-#         if not np.allclose(features_np[i], features_np[i - 1]):
-#             non_redundant_start_idx = i
-#             break
+#     # Calculate relative changes in feature values
+#     relative_changes = np.abs((features_np[1:] - features_np[:-1]) / features_np[:-1])
 
-#     # Remove redundant data
-#     t_tensor_non_redundant = t_tensor[non_redundant_start_idx:]
-#     features_tensor_non_redundant = features_tensor[non_redundant_start_idx:]
+#     # Find the index of the first spike exceeding the threshold
+#     spike_idx = np.argmax(relative_changes > spike_threshold)
 
-#     return t_tensor_non_redundant, features_tensor_non_redundant
+#     # Remove data up to the spike
+#     t_tensor_no_spikes = t_tensor[spike_idx:]
+#     features_tensor_no_spikes = features_tensor[spike_idx:]
+
+#     return t_tensor_no_spikes, features_tensor_no_spikes
 
 
-def normalize_data(features_tensor):
+def normalize_data(features_tensor: torch.Tensor) -> torch.Tensor:
     '''
-    Normalizes features between 0 and 1.
-    It has been found that normalising the data between 0 and 1 is the best for neural networks.
+    Normalizes data between 0 and 1.
     '''
     min_vals = torch.min(features_tensor, dim=0)[0]
     print("The minimum values are", min_vals)
@@ -191,7 +151,7 @@ def normalize_data(features_tensor):
     return features_tensor
 
 
-def normalize_data_mean_0(features_tensor, for_torch=True):
+def normalize_data_mean_0(features_tensor: torch.Tensor, for_torch=True) -> torch.Tensor:
     '''Normalizes features with mean 0 and std 1.'''
     if for_torch:
         mean_vals = torch.mean(features_tensor, dim=0)
@@ -204,13 +164,13 @@ def normalize_data_mean_0(features_tensor, for_torch=True):
     return features_tensor
 
 
-def get_timestep(t_tensor):
+def get_timestep(t_tensor: torch.Tensor) -> torch.Tensor:
     '''Returns the minimum timestep between each point.'''
     timestep = torch.min(t_tensor[1:] - t_tensor[:-1])
     return timestep
 
 
-def random_sampling(data: tuple[torch.Tensor, torch.Tensor], num_samples: int):
+def random_sampling(data: tuple[torch.Tensor, torch.Tensor], num_samples: int) -> tuple[torch.Tensor, torch.Tensor]:
     '''Sample given number of samples uniformly in data.'''
     t_tensor, features_tensor = data
 
@@ -252,7 +212,7 @@ def random_sampling(data: tuple[torch.Tensor, torch.Tensor], num_samples: int):
 
 # def val_shift_split(data_tuple, train_dur, val_shift, timestep=None):
 #     '''
-#     Splitting the data the same way as the function above but slightly different but idk why.
+#     Splitting the data the same way as the function above but taking validation shift instead of duration.
 #     '''
 #     t_tensor, features_tensor = data_tuple
 
@@ -272,7 +232,6 @@ def random_sampling(data: tuple[torch.Tensor, torch.Tensor], num_samples: int):
 #     return train_data, val_data, test_data
 
 
-# # This function is not used at the moment
 # def data_split(filepath, train_dur, val_dur):
 #     '''
 #     Splits the data based on measured timepoints rather than number of data points. 
@@ -296,7 +255,7 @@ def random_sampling(data: tuple[torch.Tensor, torch.Tensor], num_samples: int):
 #     return train_data, val_data, test_data
 
 
-def get_batch(data_tuple, batch_size, batch_range_idx=None, batch_range_time=None, batch_dur_idx=None, batch_dur_time=None, timestep=None, device=torch.device("cpu")):
+def get_batch(data_tuple, batch_size, batch_range_idx=None, batch_range_time=None, batch_dur_idx=None, batch_dur_time=None, timestep=None, device=torch.device("cpu")) -> tuple[torch.Tensor, torch.Tensor]:
     #maybe later improve:  when using time do math using time then convert to index to reduce rounding errors
     t_tensor, features_tensor = data_tuple
 
@@ -314,7 +273,6 @@ def get_batch(data_tuple, batch_size, batch_range_idx=None, batch_range_time=Non
             print("batch_range_idx and batch_range_time are both None, please specify one of them")
             return None
         batch_range_idx = int(batch_range_time / timestep)
-
 
     s = torch.from_numpy(np.random.choice(np.arange(batch_range_idx - batch_dur_idx, dtype=np.int64), batch_size, replace=False))
     batch_t = t_tensor[:batch_dur_idx]  # (T)
@@ -323,7 +281,7 @@ def get_batch(data_tuple, batch_size, batch_range_idx=None, batch_range_time=Non
 
 
 #SCUFFED VERSION of merts idea (unless i look at source code of odeint and change it)
-def get_batch2(data_tuple, batch_size, batch_range_idx=None, batch_range_time=None, batch_dur_idx=None, batch_dur_time=None, timestep=None, device=torch.device("cpu")):
+def get_batch2(data_tuple, batch_size, batch_range_idx=None, batch_range_time=None, batch_dur_idx=None, batch_dur_time=None, timestep=None, device=torch.device("cpu")) -> tuple[torch.Tensor, torch.Tensor]:
     #maybe later improve:  when using time do math using time then convert to index to reduce rounding errors
     t_tensor, features_tensor = data_tuple
 
@@ -341,7 +299,6 @@ def get_batch2(data_tuple, batch_size, batch_range_idx=None, batch_range_time=No
             print("batch_range_idx and batch_range_time are both None, please specify one of them")
             return None
         batch_range_idx = int(batch_range_time / timestep)
-
 
     s = torch.from_numpy(np.random.choice(np.arange(batch_range_idx - batch_dur_idx, dtype=np.int64), batch_size, replace=False))
     
@@ -351,7 +308,7 @@ def get_batch2(data_tuple, batch_size, batch_range_idx=None, batch_range_time=No
 
 
 #Merts idea but actually this time
-def get_batch3(data_tuple, batch_size, batch_range_idx=None, batch_range_time=None, batch_dur_idx=None, batch_dur_time=None, timestep=None, device=torch.device("cpu")):
+def get_batch3(data_tuple, batch_size, batch_range_idx=None, batch_range_time=None, batch_dur_idx=None, batch_dur_time=None, timestep=None, device=torch.device("cpu")) -> tuple[torch.Tensor, torch.Tensor]:
     #maybe later improve:  when using time do math using time then convert to index to reduce rounding errors
     t_tensor, features_tensor = data_tuple
 
@@ -370,51 +327,25 @@ def get_batch3(data_tuple, batch_size, batch_range_idx=None, batch_range_time=No
             return None
         batch_range_idx = int(batch_range_time / timestep)
 
-
     s = torch.from_numpy(np.random.choice(np.arange(batch_range_idx - batch_dur_idx, dtype=np.int64), batch_size, replace=False))
     batch_t = t_tensor[:batch_range_idx]  # (T)
     batch_y = torch.stack([features_tensor[s + i] for i in range(batch_dur_idx)], dim=0)  # (T, M, D)
     return s, batch_t.to(device), batch_y.to(device)
 
-# with seperate axis but not working
-# def plot_data(data_tuple):
-#     time_points = data_tuple[0].numpy()  
-#     feature_data = data_tuple[1].numpy() 
 
-#     fig, ax1 = plt.subplots(figsize=(14, 6))
-#     ax2 = ax1.twinx()
-
-#     ax1.plot(time_points, feature_data[:, 0], label='Feature 1 (speed)', color='blue')
-#     ax2.plot(time_points, feature_data[:, 1], label='Feature 2 (angle)', color='red')
-
-#     ax1.set_xlabel('Time (seconds)')
-#     ax1.set_ylabel('Feature 1 (speed)', color='blue')
-#     ax2.set_ylabel('Feature 2 (angle)', color='red')
-
-#     ax1.tick_params(axis='y', labelcolor='blue')
-#     ax2.tick_params(axis='y', labelcolor='red')
-
-#     plt.title('Features Over Time')
-#     plt.legend()
-#     plt.show()
-
-
-# train_data, val_data, test_data = val_shift_split(3, 0)
-
-#plot interpolated data
-def plot_interpolated_data(data_tuple):
+def plot_interpolated_data(data_tuple: tuple[torch.Tensor, torch.Tensor]) -> None:
+    '''Plot the interpolated data over time.'''
     t_tensor, features_tensor = interpolate_features(data_tuple[0], data_tuple[1])
     plot_data((t_tensor, features_tensor))
 
 
-# plotting input data
-def plot_data(data_tuple):
+def plot_data(data_tuple: tuple[torch.Tensor, torch.Tensor]) -> None:
+    '''Plot the input data features over time.'''
     time_points = data_tuple[0].numpy()  
     feature_data = data_tuple[1].numpy() 
     
     plt.figure(figsize=(14, 6))
 
-    
     plt.plot(time_points, feature_data[:, 0], label='Angle (Delta)') 
     plt.plot(time_points, feature_data[:, 1], label='frequency (f)')
     plt.plot(time_points, feature_data[:, 2], label='Voltage (V)')
@@ -425,38 +356,6 @@ def plot_data(data_tuple):
     plt.xlabel('Time (seconds)')
     plt.ylabel('Feature Values')
     plt.legend()
-
-
-# if __name__ == "__main__":
-#     # Add your code here
-#     savefile = False
-    
-#     # # data = load_data()
-#     # #plot_data(data)
-
-#     data = load_data_avg_duplicates()
-#     plot_interpolated_data(data)
-#     plt.show()
-
-#     #previous
-#     # data = load_data_avg_duplicates()
-#     # t_tensor, features_tensor = interpolate_features(data[0], data[1])
-#     # # Remove redundant data
-#     # t_tensor_non_redundant, features_tensor_non_redundant = remove_redundant_data(t_tensor, features_tensor)
-#     # # Plot the data after removing redundant points
-#     # plot_data((t_tensor_non_redundant, features_tensor_non_redundant))
-#     # plt.show()
-
-#     # train_data, val_data, test_data = simple_split(data, 3, 0)
-#     # train_data, val_data, test_data = val_shift_split(data, 3, .2)
-
-#     if savefile:
-#         # tf.saved_model.save(data, "real_data_scuffed1")
-
-#         #previous
-#         #torch.save((t_tensor_non_redundant, features_tensor_non_redundant), "real_data_scuffed2_non_redundant.pt")
-
-#         torch.save(data, "real_data_scuffed2.pt")
 
 
 def save_clean_raw_data(filepath: str, shift: int, start: int, file_suffix: str, normalization=False) -> str:
@@ -603,27 +502,6 @@ if __name__ == "__main__":
 
 #     if savefile:
 #         torch.save((t_tensor, features_tensor), "clean_raw_data_g1.pt")
-
-
-# if __name__ == "__main__":
-#     '''
-#     Save file of randomly sampled preprocessed data, taking clean_raw_data as input. 
-#     '''
-#     data = torch.load(filepath)
-
-#     # Random sampling
-#     t_tensor, features_tensor = random_sampling(data, data_len)
-
-#     # Normalize features
-#     features_tensor_normalized = normalize_data(features_tensor)
-#     features_tensor_normalized = normalize_data_mean_0(features_tensor)
-
-#     # # Plot the data
-#     # plot_data((t_tensor, features_tensor))
-#     # plt.show()
-
-#     if savefile:
-#         torch.save((t_tensor, features_tensor_normalized), "real_data_.pt")
 
     
 # if __name__ == "__main__":
